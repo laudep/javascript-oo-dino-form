e8e7432316cf09c2e11d7b871a3e7300666a08a7
Laurens Deprost <ldeprost@gmail.com>
2020-08-07 17:45:16 +0200

    feat: Basic functionality

--- app.js
+++ app.js
@@ -40,7 +40,6 @@ getDinoData().then((dinoData) => {
 console.log(dinos);
 
 // Create Human Object
-const human = getHumanData();
 
 // Use IIFE to get human data from form
 
@@ -78,6 +77,7 @@ function getHumanData() {
     }
 
     return {
+      species: "Human",
       name: getName(),
       height: getHeight(),
       weight: getWeight(),
@@ -90,32 +90,112 @@ function getHumanData() {
 
 // Create Dino Compare Method 1
 // NOTE: Weight in JSON file is in lbs, height in inches.
-Dino.prototype.compareWeight = function (humanWeight) {
-  const ratio = Math.floor(this.weight / humanWeight);
-  return `The ${this.name} was ${ratio} times as heavy as you.`;
+Dino.prototype.compareWeight = function (humanToCompare) {
+  const ratio = (this.weight / humanToCompare.weight).toFixed(2);
+  return ratio < 1
+    ? `The ${this.species} was only ${ratio * 100}% as heavy as you.`
+    : `The ${this.species} was ${Math.floor(ratio)} times as heavy as you.`;
 };
 
 // Create Dino Compare Method 2
 // NOTE: Weight in JSON file is in lbs, height in inches.
-Dino.prototype.compareHeight = function (humanHeight) {
-  const ratio = Math.floor(this.height / humanHeight);
-  return `The ${this.name} was ${ratio} times as tall as you.`;
+Dino.prototype.compareHeight = function (humanToCompare) {
+  const ratio = (this.height / humanToCompare.height).toFixed(2);
+  return ratio < 1
+    ? `The ${this.species} was only ${ratio * 100}% as tall as you.`
+    : `The ${this.species} was ${Math.floor(ratio)} times as tall as you.`;
 };
 
 // Create Dino Compare Method 3
 // NOTE: Weight in JSON file is in lbs, height in inches.
-Dino.prototype.compareDiet = function (humanDiet) {
-  return `The ${this.name} was a ${this.diet}${
-    this.diet === humanDiet ? " too" : ""
+Dino.prototype.compareDiet = function (humanToCompare) {
+  return `The ${this.species} was a ${this.diet}${
+    this.diet.toLowerCase() === humanToCompare.diet.toLowerCase() ? " too" : ""
   }.`;
 };
 
+// Random Dino fact method
+Dino.prototype.getRandomFact = function (humanToCompare) {
+  if (!humanToCompare) {
+    return this.fact;
+  }
+
+  if (this.species.toLowerCase() === "pigeon") {
+    return this.fact;
+  }
+
+  const getRandomNumber = (bound) => Math.floor(Math.random() * bound);
+
+  const getFact = () => this.fact;
+
+  const factFunctions = [
+    getFact,
+    this.compareWeight,
+    this.compareHeight,
+    this.compareDiet,
+  ];
+
+  return factFunctions[getRandomNumber(factFunctions.length)].call(
+    this,
+    humanToCompare
+  );
+};
+
 // Generate Tiles for each Dino in Array
+getTileHtml = (titleText, subText, image) => `
+<div class="grid-item">
+    <h3>${titleText}</h3>
+    <img src="${image}" />
+    <p>${subText}</p>
+</div>
+`;
 
 // Add tiles to DOM
+
+// Generate Tiles for each Dino in Array
+generateCompliment = async () => {
+  let compliment = await fetch("https://complimentr.com/api")
+    .then((response) => response.json())
+    .then((data) => `${data.compliment}.`)
+    .then((text) => text.charAt(0).toUpperCase() + text.slice(1))
+    .catch((e) => "");
+
+  return compliment;
+};
+
+generateHTML = async (dinos, human) => {
+  // generate dino tiles
+  const tiles = dinos.map((dino) =>
+    getTileHtml(
+      dino.species,
+      dino.getRandomFact(human),
+      `images/${dino.species.toLowerCase()}.png`
+    )
+  );
+
+  // add Human tile to the middle
+  const compliment = await generateCompliment();
+
+  tiles.splice(
+    tiles.length / 2,
+    0,
+    getTileHtml(human.name, compliment, `images/${human.image}`)
+  );
+  return tiles.join("");
+};
+// Add tiles to DOM
+
+addTilesToDOM = async (dinos, human) => {
+  document.getElementById("grid").innerHTML = await generateHTML(dinos, human);
+};
 
 // Remove form from screen
 hideForm = () =>
   (document.getElementById("dino-compare").style.display = "none");
 
 // On button click, prepare and display infographic
+document.getElementById("btn").addEventListener("click", function () {
+  const human = getHumanData();
+  addTilesToDOM(dinos, human);
+  hideForm();
+});
