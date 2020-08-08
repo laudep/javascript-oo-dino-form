84c8e42f788a12e9439cb3f6350022618d4fe034
Laurens Deprost <ldeprost@gmail.com>
2020-08-08 12:52:44 +0200

    chore: Webpack build/config

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,54 @@
 const webpack = require("webpack");
 const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ScriptExtHtmlWebpackPlugin = require("script-ext-html-webpack-plugin");
 
 module.exports = {
   mode: "development",
   entry: "./src/app.js",
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    contentBase: "./dist",
+  },
+  //   module: {
+  //     rules: [
+  //       {
+  //         test: /\.js$/,
+  //         exclude: /(node_modules)/,
+  //         use: {
+  //           loader: "babel-loader",
+  //           options: {
+  //             presets: ["@babel/preset-env"],
+  //           },
+  //         },
+  //       },
+  //     ],
+  //   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "src",
+          globOptions: {
+            dot: true,
+            // gitignore: true,
+            ignore: ["**/*.js"],
           },
         },
-      },
-    ],
-  },
-  plugins: [],
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      inject: "head",
+    }),
+    new ScriptExtHtmlWebpackPlugin({
+      defer: "bundle.js",
+    }),
+  ],
 };
