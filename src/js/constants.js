80af9b539e72624bedba3a7f20e6f376eadce91a
Laurens Deprost <ldeprost@gmail.com>
2020-08-09 18:55:00 +0200

    feat: Lower sound effect volume

--- src/js/constants.js
+++
@@ -1,5 +0,0 @@
-export const HUMAN_IMAGE = "./images/human.png";
-export const HUMAN_SOUND = "./sounds/human.wav";
-export const HUMAN_SPECIES = "Human";
-export const LOCATION_WORLD_WIDE = "World Wide";
-export const TIME_HOLOCENE = "Holocene";
